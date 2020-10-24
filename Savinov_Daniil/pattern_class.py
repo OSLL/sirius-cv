482f5fe76c2a5592a1fc94e28b7b0dca7472228a
Daniil Savinov <savinovdd@gmail.com>
2020-10-24 12:34:54 +0300

    Add files via upload

---
+++ Savinov_Daniil/pattern_class.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+
+class Pattern:
+	def __init__(self, test_img, query_img):
+		self.test_img = test_img
+		self.query_img = query_img
+
+	def addkps(self):
+		pass
+
+	def comparekps(self, test_kps, test_des, query_kps, query_des,
+		       method=cv.NORM_HAMMING, crossCheck=True, flags=2):
+		pass
