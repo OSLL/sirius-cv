37e840537f01116b6b94d8b634e00875d1e1e2a7
qmaster0803 <qmaster080305@gmail.com>
2020-12-02 19:21:54 +0300

    wip

--- Daniil_Komarov/test.py
+++ Daniil_Komarov/test.py
@@ -2,43 +2,39 @@ import numpy as np
 import cv2
 import time
 
-img2 = cv2.imread('arrow.png')          # queryImage
-img1 = cv2.imread('test_set/image.000059.jpg') # trainImage
+img1 = cv2.imread('image_q.png')          # queryImage
+img2 = cv2.imread('image_t2.jpg') # trainImage
 
-img1 = cv2.Canny(img1, 100, 200)
-img2 = cv2.Canny(img2, 100, 200)
+
+scale_percent = 200
+
+width = int(img1.shape[1] * scale_percent / 100)
+height = int(img1.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+
+
+img1 = cv2.resize(img1, dsize, interpolation=cv2.INTER_NEAREST)
 
 # Initiate SIFT detector
-sift = cv2.xfeatures2d.SURF_create()
+orb = cv2.ORB_create()
 
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
 
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-# FLANN parameters
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
+# Match descriptors.
+matches = bf.match(des1,des2)
 
-flann = cv2.FlannBasedMatcher(index_params,search_params)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
 
-matches = flann.knnMatch(des1,des2,k=2)
-
-# Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in range(len(matches))]
-
-# ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.5*n.distance:
-        matchesMask[i]=[1,0]
-
-draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = 0)
-
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], None)
 
 cv2.imshow("output", img3)
 cv2.waitKey(0)
