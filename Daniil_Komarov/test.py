13190fad04e563ca57102ab9f6b3e4693b021b6d
qmaster0803 <qmaster080305@gmail.com>
2020-12-08 22:44:39 +0300

    WIP

--- Daniil_Komarov/test.py
+++ Daniil_Komarov/test.py
@@ -1,40 +1,62 @@
-import numpy as np
-import cv2
-import time
+import cv2, numpy as np
 
-img1 = cv2.imread('image_q.png')          # queryImage
-img2 = cv2.imread('image_t2.jpg') # trainImage
+img1 = None
+#img1 = cv2.imread('./image.jpg')#, cv2.COLOR_BGR2GRAY)
+img1 = cv2.imread('stop.png')
+img2 = cv2.imread('./image_q.png')
+win_name = 'Camera Matching'
+MIN_MATCH = 10
+detector = cv2.SIFT_create(500)
+FLANN_INDEX_LSH = 6
+index_params= dict(algorithm = 0,
+                   trees = 5)
+search_params=dict(checks=50)
+matcher = cv2.FlannBasedMatcher(index_params, search_params)
+cap = cv2.VideoCapture("dt.mp4")              
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+while cap.isOpened():       
+    ret, frame = cap.read() 
+    if img1 is None: 
+        res = frame
+    else:             
+        img2 = frame
 
-scale_percent = 200
-
-width = int(img1.shape[1] * scale_percent / 100)
-height = int(img1.shape[0] * scale_percent / 100)
-
-# dsize
-dsize = (width, height)
-
-
-img1 = cv2.resize(img1, dsize, interpolation=cv2.INTER_NEAREST)
-
-# Initiate SIFT detector
-orb = cv2.ORB_create()
-
-# find the keypoints and descriptors with SIFT
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
-
-# create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-# Match descriptors.
-matches = bf.match(des1,des2)
-
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-
-# Draw first 10 matches.
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], None)
-
-cv2.imshow("output", img3)
-cv2.waitKey(0)
+        gray1 = img1#cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        gray2 = img2#cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        kp1, desc1 = detector.detectAndCompute(gray1, None)
+        kp2, desc2 = detector.detectAndCompute(gray2, None)
+        matches = matcher.knnMatch(desc1, desc2, 2)
+        ratio = 0.75
+        good_matches = [m[0] for m in matches \
+                            if len(m) == 2 and m[0].distance < m[1].distance * ratio]
+        print('good matches:%d/%d' %(len(good_matches),len(matches)))
+        matchesMask = np.zeros(len(good_matches)).tolist()
+        if len(good_matches) > MIN_MATCH: 
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ])
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ])
+            mtrx, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            accuracy=float(mask.sum()) / mask.size
+            print("accuracy: %d/%d(%.2f%%)"% (mask.sum(), mask.size, accuracy))
+            if mask.sum() > MIN_MATCH:  
+                matchesMask = mask.ravel().tolist()
+                h,w, = img1.shape[:2]
+                pts = np.float32([ [[0,0]],[[0,h-1]],[[w-1,h-1]],[[w-1,0]] ])
+                dst = cv2.perspectiveTransform(pts,mtrx)
+                img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        res = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, \
+                            matchesMask=matchesMask,
+                            flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow(win_name, res)
+    key = cv2.waitKey(1)
+    if key == 27:   
+            break          
+    elif key == ord(' '): 
+        x,y,w,h = cv2.selectROI(win_name, frame, False)
+        if w and h:
+            img1 = frame[y:y+h, x:x+w]
+else:
+    print("can't open camera.")
+cap.release()                          
+cv2.destroyAllWindows()
