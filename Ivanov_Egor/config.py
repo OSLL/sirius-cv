6c2539fc2418cefd6176e110a55cee964f20567b
Egor <sibenshtern@gmail.com>
2020-12-15 20:58:50 +0300

    intermediate commit

---
+++ Ivanov_Egor/config.py
@@ -0,0 +1,15 @@
+INDEX_PARAMETERS = {
+    "algorithm": 1,
+    "table_number": 6,
+    "key_size": 12,
+    "multi_probe_level": 1
+}
+
+SEARCH_PARAMETERS = {
+    "checks": 50
+}
+
+MIN_MATCH_COUNT = 10
+
+ROAD_SIGN = "ROAD_SIGN"
+IMAGE = "IMAGE"
--- Ivanov_Egor/test.py
+++ Ivanov_Egor/test.py
@@ -1,33 +1,83 @@
+import os
+import sys
+import argparse
+
 import cv2
-from PIL import Image
+from typing import List
 
-from analyzer import Analyzer
-
-
-def show_image_keypoints(analyzer: Analyzer) -> None:
-
-    Image.fromarray(cv2.drawKeypoints(
-        analyzer.image, analyzer.keypoints, None
-    )).show()
+from analyzer import ImageAnalyzer, PointsAnalyzer
+from config import ROAD_SIGN, IMAGE
 
 
-sign_analyzer = Analyzer(cv2.imread("sign.png"))
-test_analyzer = Analyzer(cv2.imread("test_image_1.jpg"))
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, help="path to input video")
+parser.add_argument("--output", type=str, help="path to output video")
+args = parser.parse_args()
 
-sign_analyzer.plot_points()
-test_analyzer.plot_points()
 
-# Расскомментируете следующие строки, чтобы вывести keypoints для
-# каждого изображения
-# show_image_keypoints(sign_analyzer)
-# show_image_keypoints(test_analyzer)
+signs: List[ImageAnalyzer] = []
 
-matches = sign_analyzer.compare_points(test_analyzer, method=cv2.NORM_L2)
+print("Start road sign uploading")
+index = 0
+signs_list = os.listdir(os.path.join(os.curdir, 'tst'))
+for filename in signs_list:
+    image = cv2.imread(f'tst\\{filename}')
+    image = cv2.resize(image, (image.shape[0] * 2, image.shape[1] * 2))
+    signs.append(ImageAnalyzer(image, ROAD_SIGN, filename.split('.')[0]))
+    signs[index].plot_points()
+    index += 1
 
-result_image = cv2.drawMatches(
-    sign_analyzer.image, sign_analyzer.keypoints,
-    test_analyzer.image, test_analyzer.keypoints, matches,
-    flags=2, outImg=None
-)
+    sys.stdout.write(f"\rUploaded {index} of {len(signs)} \r")
 
-Image.fromarray(result_image, 'RGB').show()
+sys.stdout.write("\rRoad signs uploading completed\n")
+
+video_capture = cv2.VideoCapture()
+video_capture.open(args.input)
+
+w = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+video_writer = cv2.VideoWriter(f"{args.output}", fourcc, 25, (w * 2, h * 2))
+
+sys.stdout.write("\rStart video processing\n")
+points_analyzer = PointsAnalyzer()
+frame_index = 0
+sys.stdout.write("\r\r")
+while video_capture.isOpened():
+    result = video_capture.read()
+    if result[0]:
+        frame = ImageAnalyzer(cv2.resize(result[1], (w * 2, h * 2)), IMAGE)
+        frame.plot_points()
+        output_image = frame.final_image.copy()
+        for sign in signs:
+            roadsigns = points_analyzer.find_roadsigns(frame, sign)
+            if roadsigns is not None:
+                for road_sign in roadsigns:
+                    x_center = int(sum(road_sign[:2]) / 2)
+                    y_center = int(sum(road_sign[2:]) / 2)
+                    add_size = int(max(
+                        (road_sign[1] - road_sign[0]),
+                        (road_sign[3] - road_sign[2]))
+                    )
+                    output_image = cv2.putText(
+                        cv2.rectangle(
+                            output_image,
+                            (x_center - add_size, y_center - add_size),
+                            (x_center + add_size, y_center + add_size),
+                            (255, 0, 0), 2
+                        ),
+                        sign.filename,
+                        (x_center - add_size, y_center - add_size),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2
+                    )
+        video_writer.write(output_image)
+
+    if result[1] is None:
+        break
+
+    frame_index += 1
+    sys.stdout.write(f"\rProcessed {frame_index} frames\r")
+
+video_capture.release()
+video_writer.release()
+sys.stdout.write("\rVideo processing completed\r")
