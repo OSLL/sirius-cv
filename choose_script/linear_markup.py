fix bug with some sign

choose_script/linear_markup.py
@@ -8,7 +8,6 @@ import numpy as np
 from modules.derivative_class_upd import CustomDetector
 
 
-
 def custom_assert(item: object, Error=AssertionError, message='') -> None or Exception:
     if not item:
         raise Error(message)
@@ -31,9 +30,8 @@ class LinearMarkup:
         self.standards = [
             Path(filepath).absolute()
             for filepath in Path(signs_path).iterdir()
-            if re.search(r'(\.jpeg)|(\.jpg)|(\.png)|(\.PNG)$', filepath)
+            if re.search(r'(\.jpeg)|(\.jpg)|(\.png)|(\.PNG)$', str(filepath))
         ]
-        print(self.standards)
 
     def img_markup(self, query_img: np.ndarray, i: int) -> None:
         """

choose_script/modules/derivative_class.py
@@ -9,7 +9,6 @@ from sklearn.cluster import DBSCAN
 from modules.pattern_class import DetectingPattern
 
 
-
 class HomographyDetector(DetectingPattern):
 
     def __init__(self, standard_paths: list) -> None:
@@ -19,7 +18,7 @@ class HomographyDetector(DetectingPattern):
             standard_path_name = Path(standard_path).name.split('.')[0]
             # standard_path_name = standard_path.split(r'/')[1].split('.')[0]
             print(standard_path)
-            self.standard_signs[standard_path_name] = cv.imread(standard_path)
+            self.standard_signs[standard_path_name] = cv.imread(str(standard_path))
 
     def add_kps(self, query_img: np.ndarray, train_img: np.ndarray) -> Tuple[list, np.ndarray, list, np.ndarray]:
         sift = cv.SIFT_create()