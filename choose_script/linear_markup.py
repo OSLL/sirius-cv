Added numbering of repeated signs, cosmetic changes, bug fixing
choose_script/linear_markup.py:
- the custom_assert function has been removed
- detector is now created in __init__
- fixed bug with saving frame with bounding box
- cosmetic changes

choose_script/modules/derivative_class_upd.py:
- self.res_coords and self.detected_signs_types is now creating in __init__
- add numbering of repeated signs

choose_script/linear_markup.py
@@ -9,12 +9,6 @@ import numpy as np
 from modules.derivative_class_upd import CustomDetector
 
 
-
-def custom_assert(item: object, Error=AssertionError, message='') -> None or Exception:
-    if not item:
-        raise Error(message)
-
-
 class LinearMarkup:
 
     def __init__(self, signs_path: str, output_folder_path='linear_markup_results', output_img_format='png'):
@@ -27,17 +21,20 @@ class LinearMarkup:
             Format in which images will be saved ('png' format by default).
         """
         self.output_folder_path = Path(output_folder_path)
-        self.output_images_folder_path = Path(self.output_folder_path, 'images')
+        self.output_images_folder_path = self.output_folder_path/'images'
         if not self.output_images_folder_path.exists():
             self.output_images_folder_path.mkdir()
 
         self.output_img_format = output_img_format
         self.markup_dict = {}
         self.standards = [
-            Path(filepath).absolute()
-            for filepath in Path(signs_path).iterdir()
+            Path(filepath).absolute() for filepath in Path(signs_path).iterdir()
             if re.search(r'(\.jpeg)|(\.jpg)|(\.png)|(\.PNG)$', str(filepath))
         ]
+        if not self.standards:
+            raise FileExistsError('No standard images were found')
+
+        self.detector = CustomDetector(self.standards)
 
     def img_markup(self, query_img: np.ndarray, i: int) -> None:
         """
@@ -46,15 +43,17 @@ class LinearMarkup:
             query_img (numpy.ndarray): Read image.
             i (integer): Index of current iteration loop.
         """
+
+        image_to_save = query_img.copy()
+
         res_img, markup = None, None
-        custom_assert(self.standards, FileExistsError, 'No standard images were found')
-        detector = CustomDetector(self.standards)
         cur_time = '-'.join(re.split(r'[ :.]', str(datetime.now())))
         img_file_name = f'img-{i + 1}__{cur_time}.{self.output_img_format}'
         try:
-            res_img, markup = detector.detect_image(query_img)
+            res_img, markup = self.detector.detect_image(query_img)
         except Exception:
-            custom_assert(res_img, NameError, 'No result image was returned')
+            if not res_img:
+                raise NameError('No result image was returned')
             raise ProcessLookupError('Detector cannot detect the image')
         else:
             if markup['regions']:
@@ -62,15 +61,12 @@ class LinearMarkup:
                 key = cv.waitKey(0)
                 if key == 50:  # "2" key -- append to the dataset
                     cv.imwrite(
-                        str(self.output_images_folder_path / img_file_name),
-                        query_img
+                        str(self.output_images_folder_path/img_file_name), image_to_save
                     )
-                    with open(self.output_folder_path / 'markup.json', 'w') as out_json:
+                    with open(self.output_folder_path/'markup.json', 'w') as out_json:
                         markup['filename'] = img_file_name
                         self.markup_dict.update(
-                            {
-                                img_file_name: markup
-                            }
+                            {img_file_name: markup}
                         )
                         json.dump(self.markup_dict, out_json, indent=4)
                     print(f'Image {img_file_name} (#{i + 1}) and markup were successfully writen')  # "2" key

choose_script/modules/derivative_class_upd.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from math import prod
 from typing import Tuple
 
@@ -8,11 +7,14 @@ import numpy as np
 from modules.derivative_class import HomographyDetector
 
 
-
 class CustomDetector(HomographyDetector):
 
-    def draw_bounding_boxes(self, cluster_pts_q: dict,
-                            query_img: np.ndarray, sign_name: str) -> Tuple[np.ndarray, list]:
+    def __init__(self, standards_paths: list):
+        super().__init__(standards_paths)
+        self.res_coords = []
+        self.detected_signs_types = []
+
+    def draw_bounding_boxes(self, cluster_pts_q: dict, query_img: np.ndarray, sign_name: str) -> Tuple[np.ndarray]:
         for pts in cluster_pts_q.values():
             pts.sort(key=lambda x: x[0])
             min_x, max_x = pts[0][0], pts[-1][0]
@@ -35,8 +37,8 @@ class CustomDetector(HomographyDetector):
         return query_img
 
     def detect_image(self, query_img: np.ndarray) -> Tuple[np.ndarray, dict]:
-        self.res_coords = []
-        self.detected_signs_types = []
+        self.res_coords.clear()
+        self.detected_signs_types.clear()
         prev_res_coords_len = 0
         for sign_name in self.standard_signs:
             train_img = self.standard_signs[sign_name]
@@ -60,13 +62,10 @@ class CustomDetector(HomographyDetector):
             'regions': {}
         }
 
-        equal_signs_counter = Counter(self.detected_signs_types)
-        for i, (sign_type, coord) in enumerate(zip(self.detected_signs_types, self.res_coords)):
-            if equal_signs_counter[sign_type] == 1:
-                name = sign_type
-            else:
-                name = f'{sign_type}-{i}'
-            #   TODO: serial number of equal sign's types
+        signs_types_count = {sign_type: 1 for sign_type in self.detected_signs_types}
+        for sign_type, coord in zip(self.detected_signs_types, self.res_coords):
+            name = f'{sign_type}-{signs_types_count[sign_type]}'
+            signs_types_count[sign_type] += 1
             markup['regions'].update(
                 {
                     name: {