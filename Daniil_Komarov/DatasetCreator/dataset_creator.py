714246346a407903eb23b73c312124998911ff78
Dan K <qmaster080305@gmail.com>
2021-06-26 12:34:24 -0700

    Task #54 solved (#60)
    * Task #54 solved
    
    * Some updates, fixes
    
    * Added save on selecting
    
    * Separated save pkl object on batch video processing
    
    * Dataset creating tool update
    
    * Fixes
    
    * Changed t-light sign
    
    * Critical fix
    
    * Fixed duplicates
    
    * Added dataset checker/balancer
    
    * Dataset creator final commit
    
    * Merge with OpenCV

---
+++ Daniil_Komarov/DatasetCreator/dataset_creator.py
@@ -0,0 +1,156 @@
+from rsd_class import RoadSignDetector
+import cv2
+import argparse
+import pickle
+import json
+import os
+import warnings
+import time
+
+def current_ms_time():
+    return round(time.time() * 1000)
+
+def _pass_func(self, *args, **kwargs): pass
+
+def processFrame(rsd, frame):
+    rsd.changeQueryImage(frame)
+    out, processed, markup = rsd.run(create_markup=True)
+    return out, processed, markup
+
+def saveFrame(raw_image, processed_image, markup, folder, filename):
+    os.makedirs(folder, exist_ok=True)
+    result = [raw_image, processed_image, markup]
+    with open(folder+"/"+filename+".dframe", "wb") as file:
+            pickle.dump(result, file, pickle.HIGHEST_PROTOCOL)
+
+def processVideo(rsd, video_folder, video, output_folder, vid_num, vids_total, total_found):
+    cap = cv2.VideoCapture(video_folder+"/"+video)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    current_frame = 1
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        try:
+            frame, processed, markup = processFrame(rsd, frame)
+            if(markup):
+                for i, proc in enumerate(processed):
+                    saveFrame(frame, proc, markup[i], output_folder, str(total_found))
+                    total_found += 1
+            print("Processing video", vid_num, "from", str(vids_total)+";", "Frame", current_frame, "from", str(total_frames)+";", "Found", total_found, "sign(s)")
+            current_frame += 1
+        except cv2.error:
+            break
+    return total_found
+
+def defineSign(rsd, sign):
+    img_test = cv2.imread(sign[1])
+    img_test = cv2.cvtColor(img_test, cv2.COLOR_BGR2GRAY)
+    rsd.addTrainImage(img_test, sign[0])
+
+def generateMarkup(frame_data, sign_name):
+    image_filename = args.output+"/"+str(current_ms_time())+".png"
+    print(image_filename, end=": ")
+    cv2.imwrite(image_filename, frame_data[0])
+    sign_markup = {"fileref": "", "size": os.path.getsize(image_filename), "filename": os.path.basename(image_filename), "base64_img_data": "", "file_attributes": {}, "regions": {}}
+    min_x = int(frame_data[2][0][0])
+    min_y = int(frame_data[2][0][1])
+    max_x = int(frame_data[2][1][0])
+    max_y = int(frame_data[2][1][1])
+    sign_markup["regions"][sign_name+'-1'] = {"shape-attributes": {"name": "polygon", "all_points_x": [min_x, max_x, max_x, min_x], "all_points_y": [min_y, min_y, max_y, max_y]}, "region-attributes": {}}
+    return sign_markup, image_filename
+
+parser = argparse.ArgumentParser(description="OpenCV dataset creator")
+parser.add_argument("action", type=str)
+parser.add_argument("input", type=str)
+parser.add_argument("output", type=str)
+
+args = parser.parse_args()
+
+warnings.warn = _pass_func
+rsd = RoadSignDetector(False)
+
+#defineSign(rsd, ("4-WAY-INTERSECT", "signs/4-way-intersect.png"))
+#defineSign(rsd, ("DUCK-CROSSING",   "signs/duck-crossing.png"))
+defineSign(rsd, ("No-left-turn",    "signs/no-left-turn.png"))
+#defineSign(rsd, ("NO-RIGHT-TURN",   "signs/no-right-turn.png"))
+#defineSign(rsd, ("PARKING",         "signs/parking.png"))
+defineSign(rsd, ("Stop",            "signs/stop.png"))
+defineSign(rsd, ("T-intersection",  "signs/t-intersection.png"))
+defineSign(rsd, ("traffic-light",   "signs/t-light-ahead.png"))
+
+if(args.action == "render"):
+    files = os.listdir(args.input)
+    videos = []
+    for file in files:
+        if(os.path.splitext(file)[1] == ".mp4"):
+            videos.append(file)
+
+    vid_num = 1
+    vids_total = len(videos)
+    total_found = 0
+
+    for video in videos:
+        total_found = processVideo(rsd, args.input, video, args.output, vid_num, vids_total, total_found)
+        vid_num += 1
+
+elif(args.action == "select"):
+    os.makedirs(args.output, exist_ok=True)
+    files = os.listdir(args.input)
+    frames = []
+    for file in files:
+        if(os.path.splitext(file)[1] == ".dframe"):
+            frames.append(file)
+
+    frames.sort(key=lambda x: int(x[:-7]))
+    output_markup = {}
+    for frame in frames:
+        print("Working on", frame, end=": ")
+        with open(args.input+"/"+frame, "rb") as file:
+            frame_data = pickle.load(file)
+        print(frame_data[2])
+        cv2.imshow("DatasetCreator", frame_data[1])
+        save_and_stop = False
+        while(True):
+                key_code = cv2.waitKey()
+                if(key_code == ord('1')):
+                    sign_markup, image_filename = generateMarkup(frame_data, 'Left-T-intersection')
+                    print("Added 'Left-T-intersection'")
+                    output_markup[os.path.basename(image_filename)] = sign_markup
+                    break
+                if(key_code == ord('2')):
+                    sign_markup, image_filename = generateMarkup(frame_data, 'T-intersection')
+                    print("Added 'T-intersection'")
+                    output_markup[os.path.basename(image_filename)] = sign_markup
+                    break
+                if(key_code == ord('3')):
+                    sign_markup, image_filename = generateMarkup(frame_data, 'Right-T-intersection')
+                    print("Added 'Right-T-intersection'")
+                    output_markup[os.path.basename(image_filename)] = sign_markup
+                    break
+
+                if(key_code == ord('d')):
+                    break
+                if(key_code == ord('s')):
+                    sign_markup, image_filename = generateMarkup(frame_data, frame_data[2][2])
+                    print("Added '"+frame_data[2][2]+"'")
+                    output_markup[os.path.basename(image_filename)] = sign_markup
+                    break
+                if(key_code == ord('e')):
+                    save_and_stop = True
+                    break
+        if(save_and_stop): break
+        else: os.remove(args.input+"/"+frame)
+
+    old_data = {}
+    if(os.path.exists(args.output+"/output.json")):
+        with open(args.output+"/output.json") as file:
+            old_data = json.loads(file.read())
+
+    old_data.update(output_markup)
+    with open(args.output+"/output.json", "w") as file:
+        file.write(json.dumps(old_data))
+
+    print("Output contains", len(old_data), "records")
+else:
+    print("Unsupported action! (render/select)")
---
+++ Daniil_Komarov/DatasetCreator/rsd_class.py
@@ -0,0 +1,114 @@
+from pattern_class import RoadSignDetectorPattern
+import cv2
+import numpy as np
+from sklearn.cluster import OPTICS
+import copy
+
+min_kps = 4
+
+class RoadSignDetector(RoadSignDetectorPattern):
+	def __init__(self, draw_kps):
+			super().__init__(draw_kps)
+
+	def detect(self, train_img):
+		sift = cv2.SIFT_create()
+		test_kps, test_des = sift.detectAndCompute(train_img, None)
+		query_img_bw = copy.copy(self.query_img)
+		query_img_bw = cv2.cvtColor(query_img_bw, cv2.COLOR_BGR2GRAY)
+		query_kps, query_des = sift.detectAndCompute(self.query_img, None)
+		return test_kps, test_des, query_kps, query_des
+
+	def compare(self, test_kps, test_des, query_kps, query_des):
+		flann = cv2.FlannBasedMatcher({"algorithm": 1, "trees": 5}, {"checks": 50})
+		matches = flann.knnMatch(test_des, query_des, k=2)
+		good_points = []
+		for m in matches:
+				if m[0].distance < 0.9*m[1].distance:
+						good_points.append(m[0])
+		if(len(good_points) > min_kps):
+			return good_points
+		else:
+			return None
+
+	def drawKps(self, query_kps, good_points):
+		x_gen = [query_kps[i.trainIdx].pt[0] for i in good_points]
+		y_gen = [query_kps[i.trainIdx].pt[1] for i in good_points]
+		coords = []
+		for i, x_coord in enumerate(x_gen):
+			cv2.circle(self.query_img, (int(x_coord), int(y_gen[i])), 7, (255, 0, 0), 2)
+		
+	def calculateSignCenters(self, query_kps, good_points):
+		if(len(good_points) > min_kps):
+			x_gen = [query_kps[i.trainIdx].pt[0] for i in good_points]
+			y_gen = [query_kps[i.trainIdx].pt[1] for i in good_points]
+			coords = []
+			for i, x_coord in enumerate(x_gen):
+				coords.append([x_coord, y_gen[i]])
+			if(self.draw_kps):
+				self.drawKps(query_kps, good_points)
+
+			points_to_clusterize = np.array(coords)
+			clust = OPTICS(min_samples=min_kps, max_eps=50)
+			clust.fit(points_to_clusterize)
+
+			roadsign_kps = []
+			i = 0
+			while(True):
+				points = points_to_clusterize[clust.labels_ == i]
+				points = points.astype(int)
+				if(points.size != 0):
+					temp = []
+					for point in points:
+						temp.append(point)
+					roadsign_kps.append(temp)
+				else:
+					break
+				i += 1
+			roadsign_coords = []
+			for kps in roadsign_kps:
+				x_kps = [i[0] for i in kps]
+				y_kps = [i[1] for i in kps]
+				roadsign_coords.append([min(x_kps), max(x_kps), min(y_kps), max(y_kps)])
+			return roadsign_coords
+		else:
+			return None
+	
+	def run(self, create_markup=False):
+		markup = []
+		processed_image = []
+		for sign in self.test_img:
+			t_kp, t_des, q_kp, q_des = self.detect(sign[0])
+			success = False
+			for i in range(3):
+				try:
+					good_points = self.compare(t_kp, t_des, q_kp, q_des)
+					success = True
+					break
+				except IndexError:
+					pass
+			if(success and good_points != None):
+				roadsign_centers = self.calculateSignCenters(q_kp, good_points)
+				for center in roadsign_centers:
+					x_size = center[1]-center[0]
+					y_size = center[3]-center[2]
+					add_size_x = int(x_size/100*40)
+					add_size_y = int(y_size/100*40)
+					processed_image.append(copy.copy(self.query_img))
+					cv2.putText(processed_image[-1], sign[1], (center[0]-add_size_x, center[2]-add_size_y-10), cv2.FONT_ITALIC, 0.4, (255,255,255), 1, cv2.LINE_AA)
+					cv2.rectangle(processed_image[-1], (center[0]-add_size_x, center[2]-add_size_y), (center[1]+add_size_x, center[3]+add_size_y), (255, 255, 255), 2)
+					markup.append([(center[0]-add_size_x, center[2]-add_size_y), (center[1]+add_size_x, center[3]+add_size_y), sign[1]])
+		if(create_markup): return self.query_img, processed_image, markup
+		else: return self.query_img, processed_image
+		
+	def changeQueryImage(self, query_img):
+		self.query_img = query_img
+
+	def addTrainImage(self, train_img, name):
+		self.test_img.append([train_img, name])
+
+	def createVideo(self, images, framerate, path):
+		height, width, layers = images[0].shape
+		video = cv2.VideoWriter(path, 0, framerate, (width, height))
+		for frame in images:
+			video.write(frame)
+		video.release()
